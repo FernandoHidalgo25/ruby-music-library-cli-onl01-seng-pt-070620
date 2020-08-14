93deb52d24ac1c0896e52a916fa77001650d3233
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-14 04:25:39 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,21 +1,20 @@
-	class MusicImporter
-
-  attr_accessor :path
+require 'pry'
+class MusicImporter
+  attr_reader :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-    files = Dir.glob("#{path}**/*")
-    files = files.collect{|file|file.scan(/(?<=mp3s\/)(.*)/)}.flatten
-  end
-
-  def import
-    new_files = self.files
-    new_files.each do |file|
-      new_song = Song.create_from_filename(file)
+    @files ||= Dir.glob("#{path}/*.mp3").collect do |file|
+      file.gsub("#{path}/", "")
     end
   end
 
+  def import
+    files.each do |file|
+      Song.create_from_filename(file)
+    end
+  end
 end
