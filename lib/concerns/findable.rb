c981161c1b08b8869dc98901396d0f8f94c21bc2
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-14 04:22:32 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,13 @@
-module Concerns
-  module Findable
+module Concerns::Findable
+
     def find_by_name(name)
-      self.all.detect {|item| item.name == name}
+      self.all.find do |item|
+        item.name == name
+      end
     end
 
     def find_or_create_by_name(name)
       self.find_by_name(name) || self.create(name)
     end
-  end
+
 end
