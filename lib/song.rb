6f32d65c01bf730bb29819ac52e1e70267cb7639
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-14 04:28:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,48 +1,19 @@
-class Song < Super
+require 'pry'
 
+class Song
+  attr_accessor :name
   attr_reader :artist, :genre
 
-  def initialize(name, artist=nil, genre=nil)
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
     @name = name
-    self.artist = artist if artist != nil
-    self.genre = genre if genre != nil
+    self.artist = artist if artist
+    self.genre = genre if genre
   end
 
   def save
-    @@all << self
-  end
-
-  def artist=(artist)
-    @artist=artist
-    artist.add_song(self)
-  end
-
-  def genre=(genre) 
-    @genre = genre
-    genre.add_song(self)
-  end
-
-  def self.create(name)
-    song = Song.new(name)
-    song.save
-    song
-  end
-
-  def self.new_from_filename(filename)
-    parms = filename.split(" - ")
-    artist = Artist.find_or_create_by_name(parms[0])
-    song = self.new(parms[1])
-    songGenre=parms[2].slice(0..parms[2].length-5)
-    genre = Genre.find_or_create_by_name(songGenre)
-    song.artist=artist
-    song.genre=genre
-    song
-  end
-
-  def self.create_from_filename(filename)
-    song=self.new_from_filename(filename)
-    song.save
-    song
+    self.class.all << self
   end
 
   def self.all
@@ -50,7 +21,44 @@ class Song < Super
   end
 
   def self.destroy_all
-    @@all.clear
+    self.all.clear
   end
 
-end 
+  def self.create(name)
+    new_song = new(name)
+    new_song.save
+    new_song
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.songs << self unless genre.songs.include?(self)
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    song_name = parts[1]
+    song_artist = parts[0]
+    song_genre = parts[2].split(".")[0]
+    artist = Artist.find_or_create_by_name(song_artist)
+    genre = Genre.find_or_create_by_name(song_genre)
+    new(song_name, artist, genre)
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).tap { |song| song.save }
+  end
+end
